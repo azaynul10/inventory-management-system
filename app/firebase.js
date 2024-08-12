a71bab8a4f9426ea68f2fdb3f70d7463ef9e6e6b
Zaynul Abedin Miah <111334392+azaynul10@users.noreply.github.com>
2024-08-12 21:27:34 +0600

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -1,13 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCfNlUvkDkL7qK5RzdzH2c-LA9sYOnBFD0",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "inventory-managment-31b37.firebaseapp.com",
   projectId: "inventory-managment-31b37",
   storageBucket: "inventory-managment-31b37.appspot.com",
